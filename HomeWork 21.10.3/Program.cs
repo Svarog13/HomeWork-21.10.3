69c8aad751e2b4778606f354e5d3e6ac8c28bbee
Void <Void@DESKTOP-LJJ5G9O>
2023-10-21 18:21:11 +0300

    Add project files.

---
+++ HomeWork 21.10.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a text: ");
+        string input = Console.ReadLine();
+
+        string[] sentences = input.Split('.');
+
+        for (int i = 0; i < sentences.Length; i++) 
+        { 
+            if (!string.IsNullOrEmpty(sentences[i]))
+            {
+                sentences[i] = sentences[i].Trim();
+                if (!string.IsNullOrEmpty(sentences[i]))
+                {
+                    char[] sentenceChars = sentences[i].ToCharArray();
+                    sentenceChars[0] = char.ToUpper(sentenceChars[0]);
+                    sentences[i] = new string(sentenceChars);
+                }
+            }
+        }
+        string result = string.Join(". ", sentences);
+        Console.Write("Result: " + result);
+    }
+}
